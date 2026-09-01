845d58560e46acb5c58297a290f3bfb365685465
imajaygiri <imlearningcoding2024@gmail.com>
2026-09-01 23:45:35 +0530

    wrote basic bootloader

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -19,7 +19,7 @@ bdb_heads:                      dw 2
 bdb_hidden_sectors:             dd 2 
 bdb_large_sectors_count:        dd 0
 
-ebr_driver_number:              db 0
+ebr_drive_number:              db 0
                                 db 0
 
 ebr_signature:                  db 29H
@@ -34,7 +34,30 @@ main:
     mov ds,ax
     mov es,ax
     mov ss,ax
-    mov sp, ORIGIN_MEM
+    mov sp, ORIGIN_MEM ; moved sp to 0x7c00
+
+	;    AH = 02
+	; AL = number of sectors to read	(1-128 dec.)
+	; CH = track/cylinder number  (0-1023 dec., see below)
+	; CL = sector number  (1-17 dec.)
+	; DH = head number  (0-15 dec.)
+	; DL = drive number (0=A:, 1=2nd floppy, 80h=drive 0, 81h=drive 1)
+	; ES:BX = pointer to buffer
+	;
+	;
+	; on return:
+	; AH = status  (see INT 13,STATUS)
+	; AL = number of sectors read
+	; CF = 0 if successful
+	;    = 1 if error
+
+
+    mov dl, byte [ebr_drive_number]
+    mov ax, 1 ; this is lba convert it to chs
+    mov cl, 1 ; sector number 1
+    mov bx, 0x7e00 ; buffer, poiter to memory location on disk
+    call disk_read ; fxn to read disk
+
 
     mov si, os_boot_msg 
     call print
@@ -42,7 +65,93 @@ main:
 
 halt: 
     jmp halt
+
+lba_to_chs:
+    ; convert lba index to CHA form
+    ;input: LBA index in AX
+    ;output: 
+    ; cx[bits 0 - 5]: sector number
+    ; cx[bits 5-15]: cylinder
+    ; dh: head
+    push ax
+    push dx
+
+    xor dx, dx
+    ; lba index is in AX = 1
+    ; sector = (lba % sector_per_track) + 1
+    ; remainder is in dx
+    div word [bdb_sector_per_track] 
+    inc dx ; this will give sector dx(remainder) + 1(inc dx) = sector
+    mov cx, dx ; now cx stores sector
+
+
+    xor dx, dx
+    ; head: (lba / sector_per_track) % number of head
+    ; cylinder: (lba / sector_per_track) / number of head
+    ; we have (lba/sector_per_track) result in ax
+    div word [bdb_heads]
     
+    mov dh , dl ;head:  moving lower val of dx to dl
+    mov ch, al
+    shl ah, 6
+    or cl , ah ; cylinder
+
+    pop ax
+    mov dl,al
+    pop dx
+
+    ret
+
+    
+disk_read:
+    ; preserve register value 
+    push ax
+    push bx
+    push cx
+    push dx
+    push di
+
+    call lba_to_chs
+    mov ah, 0x02 ; to read disk mov 02h to ah
+    mov di, 3 ; counter to try atleast 3 times
+
+
+    
+retry:
+    stc ; set the carry as bios does not set the CF
+    int 13h ; bios read disk interupt
+    ; check CF if it succeeded in
+    jnc done_read ; if CF = 0 
+
+    call disk_reset
+    dec di ; decrement the counter
+    test di,di ; if di is zero
+    jnz retry ; if not zero
+
+fail_disk_read:
+    mov si, read_fail_msg ; mov failure_msg_string to si
+    call print
+    hlt
+    ; if some how hlt fail , jmp to infite loop
+    jmp halt
+
+disk_reset:
+    pusha
+    mov ah,0
+    stc ; to check if disk reset succeded 
+    int 13h
+    jc fail_disk_read ; if cf = 1
+    jnc retry ; jmp to retry again
+    popa
+    ret
+
+done_read:
+    pop di
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
 
 print:
      push si
@@ -68,6 +177,8 @@ print_done:
 
 
 os_boot_msg: db "Welcome to myos", 0x0d, 0x0a,0
+read_fail_msg: db "Failed to read the disk",0x0d, 0x0a,0
+
 
 times 510-($-$$) db 0x0
 dw 0xaa55
